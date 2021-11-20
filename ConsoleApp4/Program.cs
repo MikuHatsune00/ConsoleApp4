a857995828f5d45584e4af5fd8f1a602a1346fa9
Kseniia <balerinagizelle@mail.ru>
2021-11-20 18:01:51 +0300

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int N = Convert.ToInt32(Console.ReadLine());
+            int k=0;
+
+            for (int i = 1; i <= N; ++i)
+            {
+                Console.WriteLine(k = k+(2 * i - 1));
+            }
+                   
+            Console.ReadKey();
+        }
+    }
+}
